4a88b63429fa66f63d306131a51a7fc654d072d8
rcarbajsa <rcarbajso@gmail.com>
2018-03-10 19:54:52 +0100

    Inicio LEECAR

---
+++ es_int.s
@@ -0,0 +1,21 @@
+* Inicializa el SP y el PC
+        ORG     $0
+        DC.L    $8000           * Pila
+        DC.L    INICIO          * PC
+
+        ORG     $400
+
+* Definicion de equivalencias
+
+MR1A    EQU     $effc01       * de modo A (escritura)
+MR2A    EQU     $effc01       * de modo A (2▒ escritura)
+SRA     EQU     $effc03       * de estado A (lectura)
+CSRA    EQU     $effc03       * de seleccion de reloj A (escritura)
+CRA     EQU     $effc05       * de control A (escritura)
+TBA     EQU     $effc07       * buffer transmision A (escritura)
+RBA     EQU     $effc07       * buffer recepcion A  (lectura)
+ACR     EQU     $effc09       * de control auxiliar
+IMR     EQU     $effc0B       * de mascara de interrupcion A (escritura)
+ISR     EQU     $effc0B       * de estado de interrupcion A (lectura)
+
+*LEECAR
